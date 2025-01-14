ac9f8c04729be17da003cdafb6e57b8556ea6c09
Aakash Shah <121144051+vvkash@users.noreply.github.com>
2025-01-14 13:12:54 -0500

    fix

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -20,7 +20,17 @@ export const LogoTicker = () => {
           The best data, from the top sites
         </h2>
         <div className="flex overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-20 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))] ">
-          <motion.div transition={{ duration: 25, ease: "linear", repeat: Infinity, }} initial={{ translateX: 0 }} animate={{ translateX: "-50%"}} className="flex gap-16 flex-none pr-16">
+          <motion.div transition={{ duration: 20, ease: "linear", repeat: Infinity, }} initial={{ translateX: 0 }} animate={{ translateX: "-50%"}} className="flex gap-16 flex-none pr-16">
+            {images.map(({ src, alt }) => (
+              <Image 
+                key={alt}
+                alt={alt}
+                src={src}
+                width={100}
+                height={100}
+                className="object-contain"
+              />
+            ))}
             {images.map(({ src, alt }) => (
               <Image 
                 key={alt}
