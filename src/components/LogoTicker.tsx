ef1c751614684eb096eab2cf1907e555d8565250
Aakash Shah <121144051+vvkash@users.noreply.github.com>
2025-01-14 13:07:47 -0500

    logo motion

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import ninjaLogo from "../assets/images/ninja.png";
 import stanleyLogo from "../assets/images/Stanley1.png";
 import ferrari_white from "../assets/images/ferrari.png"
+import { motion } from "framer-motion";
 
 const images = [
   { src: ninjaLogo, alt: "ninja Logo" },
@@ -18,8 +19,8 @@ export const LogoTicker = () => {
         <h2 className="text-xl text-center text-white">
           The best data, from the top sites
         </h2>
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-20 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))] ">
-          <div className="flex gap-16 justify-center">
+        <div className="flex overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-20 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))] ">
+          <motion.div transition={{ duration: 25, ease: "linear", repeat: Infinity, }} initial={{ translateX: 0 }} animate={{ translateX: "-50%"}} className="flex gap-16 flex-none pr-16">
             {images.map(({ src, alt }) => (
               <Image 
                 key={alt}
@@ -30,7 +31,17 @@ export const LogoTicker = () => {
                 className="object-contain"
               />
             ))}
-          </div>
+            {images.map(({ src, alt }) => (
+              <Image 
+                key={alt}
+                alt={alt}
+                src={src}
+                width={100}
+                height={100}
+                className="object-contain"
+              />
+            ))}
+          </motion.div>
         </div>
       </div>
     </div>
